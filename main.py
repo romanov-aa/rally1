d14ef86ccdcd9ce6113e0b1afd5be865260d6feb
Romanov-aa <LexaAkm@mail.ru>
2020-11-05 17:28:27 +1000

    Машинки

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 import os
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
@@ -8,17 +9,17 @@ GRAY = (128, 128, 128)
 GREEN = (0, 128, 0)
 WHITE = (200, 200, 200)
 
-bg_image = pg.image.load('Image/road.jpg')
-bg_image_rect = bg_image.get_rect(topleft=(0, 0))
-bg_image_2_rect = bg_image.get_rect(topleft=(0, 600 - HEIGT))
-car1_image = pg.image.load('Image/car1.png')
+# bg_image = pg.image.load('Image/road.jpg')
+# bg_image_rect = bg_image.get_rect(topleft=(0, 0))
+# bg_image_2_rect = bg_image.get_rect(topleft=(0, 600 - HEIGT))
+cars = [pg.image.load('Image/car1.png'), pg.image.load('Image/car2.png'), pg.image.load('Image/car3.png')]
 
 
 pg.init()
 pg.display.set_caption('Rally ')
 screen = pg.display.set_mode(SIZE)
 
-FPS = 144
+FPS = 120
 clock = pg.time.Clock()
 
 
@@ -26,42 +27,73 @@ class Player(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.image.load('Image/car4.png')
+        self.orig_image = self.image
+        self.angle = 0
+        self.speed = 2
+        self.acceleration = 0.02
+        self.x, self.y = WIDTH // 2, HEIGT // 2
+        self.rect = self.image.get_rect()
+        self.position = pg.math.Vector2(self.x, self.y)
+        self.velocity = pg.math.Vector2()
+   
+    def update(self):
+        self.image = pg.transform.rotate(self.orig_image, self.angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
+        self.position += self.velocity
+        self.rect.center = self.position
 
 
 class Car(pg.sprite.Sprite):
-    def __init__(self, x, y, w, h, s, img):
+    def __init__(self, x, y, img):
         pg.sprite.Sprite.__init__(self)
-        self.image = img
-        self.image = pg.transform.flip(car1_image, False, True)
-        self.image = pg.image.load('Image/car1.png')
-        self.x, self.y = x, y
-        self.w, self.h = w, h
-        self.speed = s
-        self.rect = self.image.get_rect(center=(self.x, self.y))
+        
+        self.image = pg.transform.flip(img, False, True)
+        # self.image = pg.image.load('Image/car1.png')        
+        # self.w, self.h = w, h
+        self.speed = random.randint(2, 3)
+        self.rect = self.image.get_rect(center=(x, y))
 
 
-play = Player()
-play_image = play.image
-play_w, play_h = play.image.get_width(), play.image.get_height()
-play.x, play.y = (WIDTH - play_w) // 2, (HEIGT - play_h) // 2
+class Road(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+
+        self.image = pg.Surface(screen.get_size())
+        self.image.fill(GRAY)
+        pg.draw.line(self.image, GREEN, (20, 0), (20, 600), 40)
+        pg.draw.line(self.image, GREEN, (780, 0), (780, 600), 40)
+        for xx in range(10):
+            for yy in range(10):
+                pg.draw.line(
+                    self.image, WHITE, 
+                    (40 + xx * 80, 0 if xx == 0 or xx == 9 else 10 + yy * 60),
+                    (40 + xx * 80, 600 if xx == 0 or xx == 9 else 50 + yy * 60), 5
+                )
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.speed = 1
+    
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.top >= HEIGT:
+            self.rect.bottom = 0
 
 
-car1 = Car(WIDTH // 2 + 80, (HEIGT - car1_image.get_height()) // 2, car1_image.get_width(), car1_image.get_height(), 1, car1_image)
-car1_image = car1.image
-car1_w, car1_h = car1.image.get_width(), car1.image.get_height()
-car1.x, car1.y = (WIDTH - car1_w) // 2 + 80, (HEIGT - car1_h) // 2
-
-
-def bg():
-    pg.draw.line(screen, GREEN, (20, 0), (20, 600), 40)
-    pg.draw.line(screen, GREEN, (780, 0), (780, 600), 40)
-    for xx in range(10):
-        for yy in range(10):
-            pg.draw.line(
-                screen, WHITE, 
-                (40 + xx * 80, 0 if xx == 0 or xx == 9 else 10 + yy * 60),
-                (40 + xx * 80, 600 if xx == 0 or xx == 9 else 50 + yy * 60), 5
-            )
+all_sprite = pg.sprite.Group()
+for r in range(2):
+    all_sprite.add(Road(0, 0 if r == 0 else -HEIGT))
+player = Player()
+list_x = []
+n = 0
+while n < 6:
+    x = random.randrange(80, WIDTH, 80)
+    if x in list_x:
+        continue
+    else:
+        list_x.append(x)
+        all_sprite.add(Car(x, -cars[0].get_height(), random.choice(cars)))
+        n += 1
+# car1_w, car1_h = car1.image.get_width(), car1.image.get_height()
+all_sprite.add(player)
 
 
 game = True
@@ -70,24 +102,11 @@ while game:
     for e in pg.event.get():
         if e.type == pg.QUIT:
             game = False
-        
-
-    bg_image_rect.y += 1
-    if bg_image_rect.y > HEIGT:
-        bg_image_rect.y = 0
-    bg_image_2_rect.y += 1
-    if bg_image_2_rect.y > 0:
-        bg_image_2_rect.y = -HEIGT
-    
-    screen.fill(GRAY)
-    #bg()
-    for i in range(2):
-        screen.blit(bg_image, bg_image_rect if i == 0 else bg_image_2_rect)
-    #screen.blit(bg_image, bg_image_2_rect)
-    screen.blit(car1_image, car1.rect)
-    screen.blit(play_image, (play.x, play.y))
+               
+    all_sprite.update()
+    all_sprite.draw(screen)
     pg.display.update()
     clock.tick(FPS)
     pg.display.set_caption(f'Rally   FPS: {int(clock.get_fps())}')
 
-#pg.image.save(screen, 'road.jpg')
+# pg.image.save(screen, 'road.jpg')
